8ad8dec095b697e9f653e2e10747f7ad5372bbd4
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-01-18 01:11:47 -0300

    Solve 'Computer Science' exercises

---
+++ 01-IntroToComputerScience/CS.js
@@ -0,0 +1,38 @@
+
+function BinarioADecimal(num) {
+    // tu codigo aca
+    let suma = 0;
+    
+    for(var potencia=num.length-1; potencia>=0; potencia--) {
+        lugarEnArray = (num.length-1) - potencia;
+        if (num[lugarEnArray] == 1) {
+            suma += Math.pow(2, potencia);
+        }
+    }
+
+    return suma;
+}
+console.log(BinarioADecimal('111010100111'));
+
+//------------------------------------------------------
+
+function DecimalABinario(num) {
+    // tu codigo aca
+    let resto = 0; 
+    let concatenacion = '';
+    
+    while (num > 0) {
+
+        if (num == 100) {
+            let numero = 50;
+        }
+        resto = num % 2;
+        num = Math.floor(num / 2);
+        concatenacion = resto + concatenacion;
+    }
+
+    return concatenacion;
+}
+
+console.log(DecimalABinario(99));
+
