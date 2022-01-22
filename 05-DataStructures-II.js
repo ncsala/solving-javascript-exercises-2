259891d328fbd5e4c360570cfbd0394e0a3ebfec
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-01-22 19:02:56 -0300

    Subiendo linked-list

---
+++ 05-DataStructures-II.js
@@ -0,0 +1,89 @@
+"use strict";
+// Implementa la clase LinkedList
+// tiene metodos `add`, `remove`, y `search`
+// add: Agrega un nuevo nodo en el final de la lista
+// Ej:      Head --> null
+// add(1):  Head --> 1 --> null
+// add(2):  Head --> 1 --> 2 --> null
+// remove:  Elimina el último nodo de la lista y devuelve su valor. (Tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía)
+// Ej:         Head --> 1
+// remove():   Head --> null y devuelve 1
+// search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
+
+
+function LinkedList() {
+    this._length = 0;
+    this.head = null;
+}
+
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+LinkedList.prototype.add = function (value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+        // si la cabeza esta vacía
+        this.head = newNode;
+        this._length++;
+    } else {
+        // si la cabeza no esta vacía
+        let current = this.head; // lo definimos con lo que tenga en la cabeza
+        while (current.next) {
+            // mientras existan otros nodos en la propiedad next
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+};
+
+LinkedList.prototype.remove = function (value) {
+    if (!this.head) return null; // cuando no existen nodos
+    if (!this.head.next) { // cuando existe un solo nodo
+        const value = this.head.value;
+        this.head = null;
+        return value;
+    }
+    let current = this.head;
+    while (current.next.next) { // cuando la lista tiene mas de un nodo // si hay dos nodos siguiente // current.next.next = null
+        current = current.next; // me quedo con el penultimo nodo, el que no tiene null
+    }
+    value = current.next.value;
+    current.next = null;
+    return value;
+    // this._length--;
+};
+
+LinkedList.prototype.search = function (valueOrCallBack) {
+    if (!this.head) return null;
+    if (typeof valueOrCallBack === 'function') {
+        if (valueOrCallBack(this.head.value)) return this.head.value; // cuando encuentra el valor buscado con la funcion
+    } else {
+        if (this.head.value === valueOrCallBack) return this.head.value; // cuando encuentra la string igual al value
+    }
+    let current = this.head;
+    while (current.next) {
+        current = current.next;
+        if (typeof valueOrCallBack === 'function') {
+            if (valueOrCallBack(current.value)) return current.value;
+        } else {
+            if (current.value === valueOrCallBack) return current.value;
+        }
+    }
+    return null;
+};
+
+// Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
+// Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
+// Para este ejercicio, generar 35 buckets para la Hash Table, y realizar los métodos, get, hasKey
+// Para almacenar un valor asociado a una key (string):
+//    - Se pasa ese valor a la función hash(Pista: usar la función charCodeAt), que determina la posición en que debe ir en el arreglo.
+//    - Luego el elemento se inserta(llamando al método set) en la posición(índice) devuelta.
+// Para buscar el valor por su key:
+//    - Sólo habrá que pasarle a la función hash la clave del elemento a buscar y ésta determinará la posición
+//      en que se encuentra.
+//    - Usar el número obtenido, para buscar(llamando al método get) el contenedor o bucket donde está el valor.
+//    - Retornar dicho valor.
+
+function HashTable() { }
---
+++ teorico-ejemplos/data-sctructures/linked-lists.js
@@ -0,0 +1,82 @@
+// Linked-lists:  nodo1---> nodo2---> nod3
+
+
+// se define el nodo. Toma como parametro data que es el valor a ingresar al nodo.
+// ejemplo: const nuevoNodo = new Node('Nicolas') ---> Node { data: 'Nicolas', next: null } 
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+// se define la lista con su cabecera en null
+// ejemplo: const nuevaLista = new List() ----> List { _length: 0, head: null }
+function List() {
+    this._length = 0;
+    this.head = null;
+}
+
+// Ej. List.add('Nicolas'); 
+List.prototype.add = function (data) {
+    const node = new Node(data); // se crea una instancia del nodo. // Ej. {data: 'Nicolas', next: null}
+    let current = this.head; // se inicializa current con lo que tenga en la cabecera // Ej. Null
+
+    // SI LA LISTA ESTÁ VACÍA, o sea si head es igual a null
+    // recordar que antes inicializamos current con el valor de head
+    if (current === null) { // otra forma es: if(!current) 
+        // Agregamos el primer nodo
+        this.head = node; // ahora el nodo que creamos va a ser la head // ej. 'Nicolas'
+        this._length++; // aumentamos el largo de la lista
+        return node;
+    }
+
+    // SI LA LISTA NO ESTÁ VACÍA, se recorre hasta encontrar el último nodo
+    while (current.next !== null) { // mientras en next haya valores // tmb se puede hacer solo (current.next)
+        current = current.next; // vamos actualizando current
+    }
+
+    current.next = node; // agrega un nodo a lo ultimo, o sea agrega al nodo que creamos al principio 'Nicolas'
+    this._length++;
+    return node
+}
+
+const miLista = new List();
+miLista;
+miLista.add('Nicolas');
+miLista;
+miLista.add('Agustina');
+miLista;
+miLista.add('Tincho');
+console.log(miLista.head);
+
+
+//---------------------------------------------------------------------------------------------------
+// Otra posibilidad para el método add
+
+function LinkedList() {
+    this._length = 0;
+    this.head = null;
+}
+
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+LinkedList.prototype.add = function (value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+        // si la cabeza esta vacía
+        this.head = newNode;
+        this._length++;
+    } else {
+        // si la cabeza no esta vacía
+        let current = this.head; // lo definimos con lo que tenga en la cabeza
+        while (current.next) {
+            // mientras existan otros nodos en la propiedad next
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+};
+
+
