4048912c969544647f5963ff971d3615b84c4ba1
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-01-24 19:40:39 -0300

    Mas de recursividad

---
+++ teorico-ejemplos/advanced-javascript/recursivas.js
@@ -0,0 +1,53 @@
+// Recordar que simpre la ultima función en ejecutarse, es la primera en devolver el valor.
+
+
+// ejemplo 1
+//----
+function contar(n) {
+  if(n == 0) return; // siempre tiene que haber un condicional para detener la recursividad
+
+  console.log(n); // la operacion que se quiere realizar en cada iteración
+
+  contar(n - 1); // llamar a nuestra función 
+}
+
+// contar(10);
+//----
+
+// ejemplo 2
+//--------
+
+//--------
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
+
+let c = new Node('C:/');
+let users = new Node('Users');
+let system = new Node('System');
+
+let file1 = new Node('file.txt');
+let file2 = new Node('imagen.jpg');
+let file3 = new Node('file.doc');
+
+c.left = users;
+c.right = system;
+
+users.left = file1;
+users.right = file2;
+
+system.left = file3;
+
+function recorrido(node) {
+  if(node == null) return;
+
+  console.log(node.data);
+
+  recorrido(node.left);
+  recorrido(node.right);
+}
+
+recorrido(c);
+//----
