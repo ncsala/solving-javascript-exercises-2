383f5cbf441eb7e00ab1ae799edf81468413549d
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-01-25 22:52:12 -0300

    Algoritmos de ordenamiento

--- 07 - Algorithms.js
+++ 07 - Algorithms.js
@@ -4,7 +4,6 @@ function selection(ar) {
   let aux = 0;
 
   while (positionA < ar.length - 1) {
-    
     if (ar[positionM] < ar[positionA]) {
       aux = ar[positionA];
       ar[positionA] = ar[positionM];
@@ -15,13 +14,13 @@ function selection(ar) {
       positionA += 1;
       positionM = positionA + 1;
     }
-    // console.log(positionA,positionM);
-    if (positionM != (ar.length-1)) {
-    positionM += 1;
+    else if (positionM < (ar.length-1)) {
+      positionM += 1;
     }
   }
 
   return ar;
 }
-            //                          8   9   length = 10
-console.log(selection([10, -2, -7, 4]));
+
+// console.log(selection([1, 24, 10, -55, 4, 3, 30, 73,-100, 100, 23, 62, 15, 80,2]));
+console.log(selection([24, 10, -55, 23,22,21,20, 2]));
