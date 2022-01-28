cd4e7d1ba8a4035ffda0ce08e2901bc7e5044363
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-01-27 23:59:55 -0300

    Selection Sort

--- 07 - Algorithms.js
+++ 07 - Algorithms.js
@@ -1,26 +1,28 @@
-function selection(ar) {
-  let positionA = 0;
-  let positionM = 1;
-  let aux = 0;
+// function selectionSort(ar) {
+//   let positionA = 0;
+//   let positionM = 1;
+//   let aux = 0;
 
-  while (positionA < ar.length - 1) {
-    if (ar[positionM] < ar[positionA]) {
-      aux = ar[positionA];
-      ar[positionA] = ar[positionM];
-      ar[positionM] = aux;
-    }
+//   while (positionA < ar.length - 1) {
+//     if (ar[positionM] < ar[positionA]) {
+//       aux = ar[positionA];
+//       ar[positionA] = ar[positionM];
+//       ar[positionM] = aux;
+//     }
     
-    if (positionM == (ar.length-1)) {
-      positionA += 1;
-      positionM = positionA + 1;
-    }
-    else if (positionM < (ar.length-1)) {
-      positionM += 1;
-    }
-  }
+//     if (positionM == (ar.length-1)) {
+//       positionA += 1;
+//       positionM = positionA + 1;
+//     }
+//     else if (positionM < (ar.length-1)) {
+//       positionM += 1;
+//     }
+//   }
 
-  return ar;
-}
+//   return ar;
+// }
 
-// console.log(selection([1, 24, 10, -55, 4, 3, 30, 73,-100, 100, 23, 62, 15, 80,2]));
-console.log(selection([24, 10, -55, 23,22,21,20, 2]));
+// // console.log(selectionSort([1, 24, 10, -55, 4, 3, 30, 73,-100, 100, 23, 62, 15, 80,2]));
+// console.log(selectionSort([24, 10, -55, 23, -3000, -2999, -3001 ,22,21,20, 2, 1543]));
+
+
