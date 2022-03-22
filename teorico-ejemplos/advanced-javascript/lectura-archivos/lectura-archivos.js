78d7cc8c6b9d3c117e9c053962c760dbf4438965
Nicolas Caceres Sala <ncsala@gmail.com>
2022-03-22 16:05:59 -0300

    Modificiaciones archivo 'lectura-archivos.js'

--- teorico-ejemplos/advanced-javascript/lectura-archivos/lectura-archivos.js
+++ teorico-ejemplos/advanced-javascript/lectura-archivos/lectura-archivos.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
-fs.readFile('./archivo.txt', 'utf-8', (err,data) => {
-  if(err) throw err
+fs.readFile('./archivo.txt', {encoding: 'utf-8'}, (error,data) => {
+  // if(error) throw error
+  if(error) process.stdout.write(`ERROR: ${error}`)
+  process.stdout.write('-------- Datos Leídos ------- \n')
   process.stdout.write(data)
 })
