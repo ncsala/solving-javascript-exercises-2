0e265c8f54018e164c90b6fc39915732c7272f8b
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-04-03 22:10:29 -0300

    Eliminando gitignore

---
+++ prueba/prueba.js
@@ -0,0 +1,53 @@
+// comprobando si son objetos y arrays
+// var obj = {
+//   a: {
+//     a1: 10,
+//     a2: "Franco",
+//     a3: { f: "r", a: "n", c: { o: true } },
+//   },
+//   b: 2,
+//   c: [1, { a: 1 }, "Franco"],
+// };
+
+// console.log(typeof obj['b'] === 'object');
+
+//       // los arreglos devuelven true
+// console.log(!Array.isArray(obj['b']));
+
+// const, let , var
+// const arreglo = [1,2];
+
+// arreglo.push(3);
+
+// arreglo;
+
+//funciones flecha etc
+
+// function f1() {
+//   console.log('f1');
+//   // return 1;
+// }
+// let b = f1;
+// b();
+// console.log(b);
+// b();
+
+// let f1b = () => console.log('f1');
+// let a = f1b();
+// console.log({a});
+
+// this
+
+// function myName() {
+//   console.log(this.name);
+//   // return this.name;
+// }
+// console.log(myName());
+
+// template strings
+// let a = 3;
+// let b = 5;
+
+// console.log(a, "hola mundo", b);
+// console.log(a + " hola mundo " + b);
+// console.log(`${a} hola mundo ${b}`);
